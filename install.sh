39b25b930150d3bba51b6473597c43a06046e2e1
Squibid <71739600+Squibid@users.noreply.github.com>
2022-04-05 12:22:16 -0400

    removed some pkg managers and added git install
    Removed all other package managers besides: pacman, & apt-get.
    Added a warning + auto exit if not using either pacman or apt-get

--- install.sh
+++ install.sh
@@ -1,12 +1,8 @@
 #!/bin/bash
 # check pkg manager based on current distro
 declare -A osInfo;
-osInfo[/etc/redhat-release]=yum
 osInfo[/etc/arch-release]=pacman
-osInfo[/etc/gentoo-release]=emerge
-osInfo[/etc/SuSE-release]=zypp
 osInfo[/etc/debian_version]=apt-get
-osInfo[/etc/alpine-release]=apk
 
 for f in ${!osInfo[@]}; do
     if [[ -f $f ]];then
@@ -14,19 +10,16 @@ for f in ${!osInfo[@]}; do
     fi
 done
 
-#update system
-if [ $pkgMngr == 'yum' ]; then
-  sudo yum update
+#update system im too lazy to add other pkg managers
 if [ $pkgMngr == 'pacman' ]; then
   sudo pacman -Syu
-if [ $pkgMngr == 'emerge' ]; then
-  emaint -a sync && emerge --ask --verbose --update --deep --newuse @world
-if [ $pkgMngr == 'zypp' ]; then
-  sudo zypper update
+  sudo pacman -S git
 if [ $pkgMngr == 'apt-get' ]; then
   sudo apt-get update && sudo apt-get upgrade
-if [ $pkgMngr == 'apk' ]; then
-  apk update && apk add --upgrade apk-tools
+  sudo apt-get install git
+else:
+  echo "Your not running a distro with one of the package managers in the program please add it by editing the install script or create an issue on the git repo"
+  exit
 fi
 
 #compile needed packages
