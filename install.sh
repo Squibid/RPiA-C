29622bd83535beb94fe8c74df0a660a39f2d317b
Squibid <71739600+Squibid@users.noreply.github.com>
2022-04-07 12:56:55 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -13,10 +13,10 @@ done
 #update system im too lazy to add other pkg managers
 if [ $pkgMngr == 'pacman' ]; then
   sudo pacman -Syu
-  sudo pacman -S git
+  sudo pacman -S - < pkgs
 if [ $pkgMngr == 'apt-get' ]; then
   sudo apt-get update && sudo apt-get upgrade
-  sudo apt-get install git
+  cat pkgs | xargs sudo apt-get install
 else:
   echo "Your not running a distro with one of the package managers in the program please add it by editing the install script or create an issue on the git repo"
   exit
@@ -24,4 +24,8 @@ fi
 
 #compile needed packages
 git clone https://github.com/xorg62/tty-clock.git && cd tty-clock && sudo make clean install
-git clone https://github.com/dunst-project/dunst.git && cd dunst && make && sudo make install
+git clone -b gaps https://github.com/Airblader/i3.git && cd i3 && sudo make install
+
+#move essential files
+cp config ~/.config/i3/
+cp picom.conf ~/.config/
